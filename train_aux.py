18260a47f6f7ff9c5e44dbed2e6cb97e59dd35c3
ChristofHenkel <henkel.christof@googlemail.com>
2022-08-29 08:18:55 +0200

    Update train_aux.py
    fix local global inconsistency

--- train_aux.py
+++ train_aux.py
@@ -586,6 +586,8 @@ if __name__ == '__main__':
     # DDP mode
     opt.total_batch_size = opt.batch_size
     device = select_device(opt.device, batch_size=opt.batch_size)
+    #fix local global inconsistency
+    opt.local_rank = opt.global_rank
     if opt.local_rank != -1:
         assert torch.cuda.device_count() > opt.local_rank
         torch.cuda.set_device(opt.local_rank)
